2cfb456f5fdc1fb87f9d45f045c3be9fec6684b3
Shubh7codes <121547324+Shubh7codes@users.noreply.github.com>
2023-07-04 10:48:16 +0530

    GFG POTD 4 July 2023

---
+++ Count the subarrays having product less than k.cpp
@@ -0,0 +1,31 @@
+class Solution{
+  public:
+    int countSubArrayProductLessThanK(const vector<int>& a, int n, long long k) {
+        long long p = 1;
+        int res = 0;
+        for (int start = 0, end = 0; end < n; end++) {
+    
+            // Move right bound by 1 step. Update the product.
+            p *= a[end];
+    
+            /* Move left bound so guarantee that p is again
+            less than k.*/
+            
+            while (start < end && p >= k) p /= a[start++];
+            
+            /* If p is less than k, update the counter.
+            Note that this is working even for (start == end):
+            it means that the previous window cannot grow
+            anymore and a single array element is the only
+            anymore and a single array element is the only
+            */
+
+            if (p < k) {
+                int len = end - start + 1;
+                res += len;
+            }
+        }
+    
+        return res;
+    }
+};
