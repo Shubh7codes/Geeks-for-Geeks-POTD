2efed5f6c219c519a6c9eddca7aaf12fc2b0ed47
Shubh7codes <121547324+Shubh7codes@users.noreply.github.com>
2023-08-09 20:42:41 +0530

    GFG POTD 9 August 2023

---
+++ Largest prime factor.cpp
@@ -0,0 +1,14 @@
+class Solution{
+public: 
+    long long int largestPrimeFactor(int N)
+    {
+        long long num = 2;
+        // loop till sqrt(N)
+        while ((num * num) <=N)
+        if (N % num == 0) // if num divides N evenly
+            N /= num; // divide N by num
+        else
+            num++; // increase num if it don't divide N
+        return N;
+    }
+};
